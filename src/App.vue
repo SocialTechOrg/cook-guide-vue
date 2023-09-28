13ad2e87939b9b5b4e9801f542209ac094f94601
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-28 13:35:27 -0500

    chore:  project structure refactor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './authentication/components/HelloWorld.vue'
 </script>
 
 <template>
