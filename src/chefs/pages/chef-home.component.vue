17fa154635780d09ce78976602a4ea627039c125
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-28 17:55:36 -0500

    feat: Added chef-home component view

---
+++ src/chefs/pages/chef-home.component.vue
@@ -0,0 +1,29 @@
+<script>
+import Menubar from "../../shared/components/menubar.component.vue";
+
+export default {
+  name: "chef-home",
+  components: { Menubar }
+};
+
+</script>
+
+<template>
+  <Menubar></Menubar>
+</template>
+
+<style scoped>
+
+body{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+#app{
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+</style>
