ac701a6b904518f6ee52ec8be2e064bd05a67721
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-29 02:57:10 -0500

    feat: added styles

--- src/chefs/components/add-recipe-card.component.vue
+++ src/chefs/components/add-recipe-card.component.vue
@@ -1,28 +1,31 @@
 <template>
-  <form @submit.prevent="enviarReceta">
-    <div class="form-group">
-      <pv-input-text id="name" placeholder="Nombre" v-model="recipe.name" required/>
-    </div>
-    <div class="form-group">
-      <pv-input-text id="author" placeholder="Autor" v-model="recipe.author" required/>
-    </div>
-    <div class="form-group">
-      <pv-input-text id="image" placeholder="URL de imagen" v-model="recipe.image" required/>
-    </div>
-    <div class="form-group">
-      <pv-textarea id="ingredients" placeholder="Ingredientes" v-model="recipe.ingredients" rows="5" cols="30" required/>
-    </div>
-    <div class="form-group">
-      <pv-textarea id="preparation" placeholder="Preparación" v-model="recipe.preparation" rows="5" cols="30" required/>
-    </div>
-    <div class="form-group">
-      <pv-input-text id="time" placeholder="Tiempo de preparación" v-model="recipe.time" required/>
-    </div>
-    <div class="form-group">
-      <pv-input-text id="servings" placeholder="Porciones" v-model="recipe.servings" required/>
-    </div>
-    <button type="submit" class="btn btn-primary">Agregar Receta</button>
-  </form>
+  <div class="form-container">
+    <form @submit.prevent="addRecipe" class="add-form">
+      <div class="form-group">
+        <pv-input-text id="name" placeholder="Nombre" v-model="recipe.name" required/>
+      </div>
+      <div class="form-group">
+        <pv-input-text id="author" placeholder="Autor" v-model="recipe.author" required/>
+      </div>
+      <div class="form-group">
+        <pv-input-text id="image" placeholder="URL de imagen" v-model="recipe.image" required/>
+      </div>
+      <div class="form-group">
+        <pv-textarea id="ingredients" placeholder="Ingredientes" v-model="recipe.ingredients" rows="5" cols="30" required/>
+      </div>
+      <div class="form-group">
+        <pv-textarea id="preparation" placeholder="Preparación" v-model="recipe.preparation" rows="5" cols="30" required/>
+      </div>
+      <div class="form-group">
+        <pv-input-text id="time" placeholder="Tiempo de preparación" v-model="recipe.time" required/>
+      </div>
+      <div class="form-group">
+        <pv-input-text id="servings" placeholder="Porciones" v-model="recipe.servings" required/>
+      </div>
+      <pv-button class="custom-button" type="submit" label="Agregar Receta" />
+    </form>
+  </div>
+
 </template>
 
 <script>
@@ -30,7 +33,7 @@ import axios from "axios";
 
 export default {
   props: {
-    recipe: Object, // Propiedad para pasar la receta desde chef-home
+    recipe: Object,
   },
   data() {
     return {
@@ -47,41 +50,33 @@ export default {
     };
   },
   methods: {
-    enviarReceta() {
-      // Validación de los datos, por ejemplo, asegúrate de que los campos obligatorios no estén vacíos.
+    addRecipe() {
       if (!this.recipe.name || !this.recipe.author || !this.recipe.image) {
         alert("Por favor, complete los campos obligatorios.");
         return;
       }
 
-      // Crea un objeto con los datos de la receta para enviar al servidor
-      const datosReceta = {
+      const recipeData = {
         id: this.recipe.id,
         name: this.recipe.name,
         author: this.recipe.author,
         image: this.recipe.image,
-        ingredients: this.recipe.ingredients.split(','), // Convierte los ingredientes en un array
+        ingredients: this.recipe.ingredients.split(','),
         preparation: this.recipe.preparation,
         time: this.recipe.time,
         servings: this.recipe.servings
       };
 
-      // Realiza una solicitud HTTP POST a la URL del servidor
-      axios.post('http://localhost:3000/recipes', datosReceta)
+      axios.post('http://localhost:3000/recipes', recipeData)
           .then(response => {
-            // Maneja la respuesta del servidor aquí, si es necesario
             console.log('Receta enviada con éxito:', response.data);
-
-            // Luego, puedes restablecer el formulario o realizar otras acciones después del envío
-            this.resetearFormulario();
+            this.resetForm();
           })
           .catch(error => {
-            // Maneja los errores de la solicitud aquí
             console.error('Error al enviar la receta:', error);
           });
     },
-    resetearFormulario() {
-      // Aquí puedes restablecer los valores del formulario o redirigir a otra página, etc.
+    resetForm() {
       this.recipe = {
         id: '',
         name: '',
@@ -99,7 +94,32 @@ export default {
 
 
 <style>
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.add-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
 
+.custom-button {
+  background-color: #E06B43;
+  border-color: #E06B43;
+  border-radius: 20px;
+}
+
+.form-group {
+  width: 100%;
+}
+.add-form input,
+.add-form textarea {
+  width: 90%;
+  margin: 10px;
+}
 
 </style>
